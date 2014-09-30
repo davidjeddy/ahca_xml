e02affa5b199455af93c8ebef5a84332e7d85e77
David J Eddy <me@davidjeddy.com>
2014-09-29 23:31:34 -0400

    Iterated on web/index ENV and DEBUG auto detection

--- public_html/web/index.php
+++ public_html/web/index.php
@@ -1,13 +1,15 @@
 <?php
 
-// NOTE: Make sure this file is not accessible when deployed to production
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
-    die('You are not allowed to access this file.');
+// Define ENV and DEBUG options
+if (
+	in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])
+) {
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+	defined('YII_DEBUG') or define('YII_DEBUG', false);
+	defined('YII_ENV') or define('YII_ENV', 'prod');
 }
-
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
