275abf64c5a6442600914de498f9340813427b80
David J Eddy <me@davidjeddy.com>
2014-09-29 20:21:04 -0400

    Altered config files for basic Yii2 web app install.

---
+++ public_html/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ahca_state_report',
+    'username' => 'root',
+    'password' => 'Welcome123',
+    'charset' => 'utf8',
+];
