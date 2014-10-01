3aba112659ec25746cd45e558225713ee3e926b9
David J Eddy <me@davidjeddy.com>
2014-09-30 21:30:25 -0400

    Added Yii2 and HTML5 validation to all fields currently in the form.

--- public_html/views/records/_form.php
+++ public_html/views/records/_form.php
@@ -25,18 +25,37 @@ use dosamigos\datetimepicker\DateTimePicker;
 
 <div class="records-form">
 
+    <?php // @todo create a quick menu for on page navigation. - DJE : 2014-09-29; ?>
+
     <?php $form = ActiveForm::begin(); ?>
 
-    <h2><span class="label label-primary">General Patient Information:</span></h2>
-    <h2><span class="label label-primary">Charges:</span></h2>
-    <h2><span class="label label-primary">Practitioner Information:</span></h2>
-    <h2><span class="label label-primary">Dates and Times:</span></h2>
 
-    <?= $form->field($model, 'ahca_num')->textInput(['maxlength' => 10]) ?>
 
-    <?= $form->field($model, 'med_rec_num')->textInput(['maxlength' => 24]) ?>
+    <h2><span class="label label-primary">General Information:</span></h2>
 
-    <?= $form->field($model, 'ssn')->textInput(['maxlength' => 9]) ?>
+    <?= $form->field($model, 'ahca_num')->textInput([
+        'placeholder' => '8 to 10 digits',
+        'minlength'   => 8,
+        'maxlength'   => 10,
+        'step'        => 1,
+        'pattern'     => '^[0-9]{8,10}$'
+    ]); ?>
+
+    <?= $form->field($model, 'med_rec_num')->textInput([
+        'placeholder' => '24 digit alphanumeric',
+        'minlength'   => 6,
+        'maxlength'   => 24,
+        'type'        => 'mixed',
+        'pattern'     => '^[a-zA-Z0-9]*$'
+    ]); ?>
+
+    <?= $form->field($model, 'ssn')->textInput([
+        'placeholder' => 'Social Security number (no dashes)',
+        'minlength'   => 9,
+        'maxlength'   => 9,
+        'type'        => 'number',
+        'pattern'     => '^\d{9}$'
+    ]); ?>
 
     <?php //$form->field($model, 'ethnicity_id')->textInput(); ?>
     <?php
@@ -117,7 +136,32 @@ use dosamigos\datetimepicker\DateTimePicker;
     );
     ?>
 
-    <?= $form->field($model, 'zip')->textInput(['maxlength' => 5]) ?>
+    <?php //echo $form->field($model, 'patient_status_id')->textInput(); ?>
+    <?php
+    echo Html::activeLabel($model, 'patient_status');
+    echo Html::activeDropDownList(
+        $model,
+        'idc9Code',
+        ArrayHelper::map(
+            PatientStatus::find()->all(),
+            'patient_status_id',
+            'patient_status_value',
+            'patient_status_description'
+        ),
+        [
+            'prompt' => 'Pick One',
+            'class'  => 'form-control'
+        ]
+    );
+    ?>
+
+    <?= $form->field($model, 'zip')->textInput([
+        'placeholder' => '5 digit Zip code number',
+        'minlength'   => 5,
+        'maxlength'   => 5,
+        'type'        => 'number',
+        'pattern'     => '^\d{5}$'
+    ]) ?>
 
     <?php //echo $form->field($model, 'country_id')->textInput(); ?>
     <?php
@@ -138,43 +182,25 @@ use dosamigos\datetimepicker\DateTimePicker;
     );
     ?>
 
-    <?php //$form->field($model, 'visit_begin_date')->textInput(); ?>
-    <?= $form->field($model, 'visit_begin_date')->widget(DateTimePicker::className(), [
-        'language'       => 'en',
-        'size'           => 'ms',
-        'template'       => '{input}',
-        'pickButtonIcon' => 'glyphicon glyphicon-time',
-        'inline'         => false,
-        'clientOptions'  => [
-            'startView'          => 4,
-            'minView'            => 2,
-            'maxView'            => 4,
-            'autoclose'          => true,
-            'format'             => 'yyyy-mm-dd', // if inline = false
-            'keyboardNavigation' => true,
-            'forceParse'         => false,
-            'todayBtn'           => true
-        ]
-    ]);?>
 
-    <?php //$form->field($model, 'arrival_hour')->textInput(['maxlength' => 2]); ?>
-    <?= $form->field($model, 'arrival_hour')->widget(DateTimePicker::className(), [
-        'language'       => 'en',
-        'size'           => 'ms',
-        'template'       => '{input}',
-        'pickButtonIcon' => 'glyphicon glyphicon-time',
-        'inline'         => false,
-        'clientOptions'  => [
-            'startView'          => 1,
-            'minView'            => 1,
-            'maxView'            => 1,
-            'autoclose'          => true,
-            'format'             => 'hh', // if inline = false
-            'keyboardNavigation' => true,
-            'forceParse'         => false,
-            'todayBtn'           => true
+    <?php //echo $form->field($model, 'admission_source_id')->textInput(); ?>
+    <?php
+    echo Html::activeLabel($model, 'admission_source_id');
+    echo Html::activeDropDownList(
+        $model,
+        'admission_source_id',
+        ArrayHelper::map(
+            AdmissionSource::find()->all(),
+            'admission_source_id',
+            'admission_source_value',
+            'admission_source_description'
+        ),
+        [
+            'prompt' => 'Pick One',
+            'class'  => 'form-control'
         ]
-    ]);?>
+    );
+    ?>
 
     <?php //$form->field($model, 'service_id')->textInput() ?>
     <?php
@@ -189,25 +215,6 @@ use dosamigos\datetimepicker\DateTimePicker;
             'servicecode_description'
         ),
         // @todo abstract this to app settings - DJE : 2014-09-30
-        [
-            'prompt' => 'Pick One',
-            'class'  => 'form-control'
-        ]
-    );
-    ?>
-
-    <?php //echo $form->field($model, 'admission_source_id')->textInput(); ?>
-    <?php
-    echo Html::activeLabel($model, 'admission_source_id');
-    echo Html::activeDropDownList(
-        $model,
-        'admission_source_id',
-        ArrayHelper::map(
-            AdmissionSource::find()->all(),
-            'admission_source_id',
-            'admission_source_description',
-            'admission_source_value'
-        ),
         [
             'prompt' => 'Pick One',
             'class'  => 'form-control'
@@ -253,54 +260,208 @@ use dosamigos\datetimepicker\DateTimePicker;
     );
     ?>
 
-    <?= $form->field($model, 'pharmacy_charges')->textInput() ?>
 
-    <?= $form->field($model, 'med_surg_supply_charges')->textInput() ?>
 
-    <?= $form->field($model, 'lab_charges')->textInput() ?>
+    <h2><span class="label label-primary">IDC9 Information:</span></h2>
 
-    <?= $form->field($model, 'radiology_charges')->textInput() ?>
+    <?= $form->field($model, 'admitting_idc9_code_id')->textInput([
+        'placeholder' => 'IDC9 code format',
+        'pattern'     => '\b[EV]?\d{2,3}(?:\.\d{1,2})?\b'
+    ]); ?>
 
-    <?= $form->field($model, 'cardiology_charges')->textInput() ?>
+    <?= $form->field($model, 'prin_proc_code')->textInput([
+        'placeholder' => 'Valid ICD9-CM Procedure Code',
+        'type'        => 'mixed',
+        'pattern'     => '\b[EV]?\d{2,3}(?:\.\d{1,2})?\b'
+    ]); ?>
 
-    <?= $form->field($model, 'oper_room_charges')->textInput() ?>
+    <h2><span class="label label-primary">Charges:</span></h2>
 
-    <?= $form->field($model, 'anesthesia_charges')->textInput() ?>
+    <?= $form->field($model, 'anesthesia_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'cardiology_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'er_room_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'extra_shock_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'gi_services_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'lab_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'med_surg_supply_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'oper_room_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'other_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'pharmacy_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'radiology_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+          'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'recovery_room_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'total_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
+    
+    <?= $form->field($model, 'trauma_resp_charges')->textInput([
+        'minlength' => 1,
+        'maxlength' => 11,
+        'type'      => 'number',
+        'pattern'   => '^[0-9]{11}*$'
+    ]); ?>
 
-    <?= $form->field($model, 'recovery_room_charges')->textInput() ?>
 
-    <?= $form->field($model, 'er_room_charges')->textInput() ?>
 
-    <?= $form->field($model, 'trauma_resp_charges')->textInput() ?>
+    <h2><span class="label label-primary">Practitioner(s) Information:</span></h2>
 
-    <?= $form->field($model, 'gi_services_charges')->textInput() ?>
+    <?= $form->field($model, 'attending_pract_id')->textInput([
+        'minlength' => 12,
+        'maxlength' => 12,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
 
-    <?= $form->field($model, 'extra_shock_charges')->textInput() ?>
+    <?= $form->field($model, 'attending_pract_npi')->textInput([
+        'minlength' => 10,
+        'maxlength' => 10,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
 
-    <?= $form->field($model, 'other_charges')->textInput() ?>
+    <?= $form->field($model, 'operating_pract_id')->textInput([
+        'minlength' => 12,
+        'maxlength' => 12,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
 
-    <?= $form->field($model, 'total_charges')->textInput() ?>
+    <?= $form->field($model, 'operating_pract_npi')->textInput([
+        'minlength' => 10,
+        'maxlength' => 10,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
 
-    <?= $form->field($model, 'admitting_idc9_code_id')->textInput() ?>
+    <?= $form->field($model, 'other_pract_id')->textInput([
+        'minlength' => 12,
+        'maxlength' => 12,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
 
-    <?php //echo $form->field($model, 'patient_status_id')->textInput(); ?>
-    <?php
-    echo Html::activeLabel($model, 'patient_status');
-    echo Html::activeDropDownList(
-        $model,
-        'idc9Code',
-        ArrayHelper::map(
-            PatientStatus::find()->all(),
-            'patient_status_id',
-            'patient_status_value',
-            'patient_status_description'
-        ),
-        [
-            'prompt' => 'Pick One',
-            'class'  => 'form-control'
+    <?= $form->field($model, 'other_pract_npi')->textInput([
+        'minlength' => 10,
+        'maxlength' => 10,
+        'type'      => 'mixed',
+        'pattern'   => '^\d{a-zA-z0-9}$'
+    ]); ?>
+
+
+
+    <h2><span class="label label-primary">Dates and Times:</span></h2>
+
+    <?php //$form->field($model, 'visit_begin_date')->textInput(); ?>
+    <?= $form->field($model, 'visit_begin_date')->widget(DateTimePicker::className(), [
+        'language'       => 'en',
+        'size'           => 'ms',
+        'template'       => '{input}',
+        'pickButtonIcon' => 'glyphicon glyphicon-time',
+        'inline'         => false,
+        'clientOptions'  => [
+            'startView'          => 4,
+            'minView'            => 2,
+            'maxView'            => 4,
+            'autoclose'          => true,
+            'format'             => 'yyyy-mm-dd', // if inline = false
+            'keyboardNavigation' => true,
+            'forceParse'         => false,
+            'todayBtn'           => true
         ]
-    );
-    ?>
+    ]);?>
+
+    <?php //$form->field($model, 'arrival_hour')->textInput(['maxlength' => 2]); ?>
+    <?= $form->field($model, 'arrival_hour')->widget(DateTimePicker::className(), [
+        'language'       => 'en',
+        'size'           => 'ms',
+        'template'       => '{input}',
+        'pickButtonIcon' => 'glyphicon glyphicon-time',
+        'inline'         => false,
+        'clientOptions'  => [
+            'startView'          => 1,
+            'minView'            => 1,
+            'maxView'            => 1,
+            'autoclose'          => true,
+            'format'             => 'hh', // if inline = false
+            'keyboardNavigation' => true,
+            'forceParse'         => false,
+            'todayBtn'           => true
+        ]
+    ]);?>
 
     <?php //$form->field($model, 'visit_end_date')->textInput(); ?>
     <?= $form->field($model, 'visit_end_date')->widget(DateTimePicker::className(), [
@@ -321,18 +482,6 @@ use dosamigos\datetimepicker\DateTimePicker;
         ]
     ]);?>
 
-    <?= $form->field($model, 'attending_pract_npi')->textInput(['maxlength' => 10]) ?>
-
-    <?= $form->field($model, 'operating_pract_npi')->textInput(['maxlength' => 10]) ?>
-
-    <?= $form->field($model, 'other_pract_npi')->textInput(['maxlength' => 10]) ?>
-
-    <?= $form->field($model, 'attending_pract_id')->textInput(['maxlength' => 12]) ?>
-
-    <?= $form->field($model, 'operating_pract_id')->textInput(['maxlength' => 12]) ?>
-
-    <?= $form->field($model, 'other_pract_id')->textInput(['maxlength' => 12]) ?>
-
     <?php //$form->field($model, 'ed_discharge_hour')->textInput(['maxlength' => 2]); ?>
     <?= $form->field($model, 'ed_discharge_hour')->widget(DateTimePicker::className(), [
         'language'       => 'en',
@@ -351,8 +500,6 @@ use dosamigos\datetimepicker\DateTimePicker;
             'todayBtn'           => true
         ]
     ]);?>
-
-    <?= $form->field($model, 'prin_proc_code')->textInput(['maxlength' => 8]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
