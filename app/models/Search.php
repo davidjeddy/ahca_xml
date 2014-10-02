217a088258ebd8642e58009c3835f7b72bb68f43
David J Eddy <me@davidjeddy.com>
2014-10-01 20:30:47 -0400

    Renamed MDL 'search' to 'Search' In said MDL changed 'records' namespace to 'Records' as it should be.

--- app/models/Search.php
+++ app/models/Search.php
@@ -5,7 +5,7 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\records;
+use app\models\Records;
 
 /**
  * search represents the model behind the search form about `app\models\records`.
