6c9ddf6ac4ecf8123cac2fd2f0627a579346f36f
David J Eddy <me@davidjeddy.com>
2014-10-08 00:14:18 -0400

    One to many zero's there buddy.

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -216,7 +216,7 @@ class ReportsController extends \yii\web\Controller
             
             // @todo quick stop-gap for non United States patience
             if ($r_value['country']['country_id'] != 184) {
-                $_method_data[$r_key]['PATIENT_SSN'] = '00009';
+                $_method_data[$r_key]['PATIENT_SSN'] = '0009';
             }
 
             $_method_data[$r_key]['PATIENT_ETHNICITY'] = $r_value['ethnicity']['ethnicity_value'];
